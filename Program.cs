38338c16d4aa4bc36a607fcb754973154723b833
Kiss Tamás <Tamas.Kiss.87@hotmail.com>
2021-08-28 20:19:48 +0200

    created UpdateDatabase method

--- Program.cs
+++ Program.cs
@@ -10,22 +10,31 @@ namespace quizgame
         static void Main(string[] args)
         {
             string dataBase = Path.Combine(Directory.GetCurrentDirectory(), "Database.txt");
-            Console.WriteLine(dataBase);
             if ((File.Exists(dataBase)) == false)
             {
                 using (File.Create(dataBase));
             }
             int lineCount = File.ReadLines(dataBase).Count();
-            StreamWriter sw = new StreamWriter(dataBase, true, Encoding.ASCII);
             UI.WelcomeMessage();
             UI.CurrentQuestions(lineCount);
             bool wantToAdd = UI.AddQuestionRequest();
             while (wantToAdd)
             {
-                var question = UI.AddQuestion();
-                sw.WriteLine(question);
+                UpdateDatabase(dataBase);               
                 wantToAdd = UI.AddQuestionRequest();
             }
+            
+        }
+
+        /// <summary>
+        /// Adding questions to the database
+        /// </summary>
+        /// <param name="path">path of the database</param>
+        public static void UpdateDatabase(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, true, Encoding.ASCII);
+            var question = UI.AddQuestion();
+            sw.WriteLine(question);
             sw.Close();
         }
 
